a6d858dc1c52d6ac8a178674c7f53dc5d1704e1d
leondbtune <97840713+leondbtune@users.noreply.github.com>
2022-08-03 01:35:25 +0200

    first commit

---
+++ from bs4 import BeautifulSoup.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+page_to_scrape = requests.get("https://www.procyclingstats.com/rider/greg-van-avermaet")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+name = soup.find_all('title', limit=1)
